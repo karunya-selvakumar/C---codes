4752b192ab1afe588173f1893ed47631ce0eee52
karunya-selvakumar <karunyaselva03@gmail.com>
2026-04-06 09:44:07 +0530

    Add files via upload

---
+++ exception-programs/oddeven.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+    try
+    {
+        if(x%2!=0)
+        {
+          throw -1;
+        }
+        cout<<"Even number!";
+    }
+    catch(int e)
+    {
+        cout<<"Odd number caught!";
+    }
+    return 0;
+}
